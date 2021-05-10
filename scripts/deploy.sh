47f21765d8b515ee0b61317d01b6d1ea988eeae3
catid <mrcatid@gmail.com>
2021-05-10 06:08:42 +0000

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 ServerList=("ny" "sf" "lon" "jpn" "aus")
 
-cd ..
 npm run release
-npm run minify
-cd scripts
+npm run client
 
 for server in ${ServerList[*]}; do
     echo "Working on $server..."
@@ -13,7 +11,7 @@ for server in ${ServerList[*]}; do
     ssh $server rm -rf /var/www/bossballoon.io/ && mkdir /var/www/bossballoon.io
 
     echo "Copying new www"
-    rsync -avP --files-from=www_file_list.txt ../client/deploy/ $server:/var/www/bossballoon.io/
+    rsync -avP --files-from=www_file_list.txt client/deploy/ $server:/var/www/bossballoon.io/
 
     echo "Deleting old server"
     ssh $server pkill node && rm -rf server && mkdir server
@@ -22,6 +20,6 @@ for server in ${ServerList[*]}; do
     rsync -avP --files-from=server_file_list.txt .. $server:~/server/
 
     echo "Installing server"
-    ssh $server cd ~/bossballoon && npm install --production && npm run server
+    ssh $server cd ~/bossballoon && npm run ubuntu && npm run server
 done
 
