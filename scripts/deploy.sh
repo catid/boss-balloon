82bafa285b864009c0d05675fde15b0bb2ad39e6
Catid <mrcatid@gmail.com>
2021-10-11 00:28:50 -0700

    wip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,4 +16,7 @@ for server in ${ServerList[*]}; do
 
     echo "Copying new server"
     rsync -avP --files-from=scripts/server_file_list.txt . $server:~/server/
+
+    echo "Starting new server"
+    ssh $server pm2 restart 0
 done
